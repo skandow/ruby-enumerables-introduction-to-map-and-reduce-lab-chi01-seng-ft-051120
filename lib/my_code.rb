1862318b21037e315d51648c86b8eceba8cf7613
Steven Kandow <kando1sr@gmail.com>
2020-03-16 16:31:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -34,4 +34,12 @@ def map_to_square(source_array)
 end 
 
 def reduce_to_total(source_array)
+  result = 0 
+  array_count = 0 
+  while array_count < source_array.length do
+    result += source_array[array_count]
+    array_count += 1 
+  end 
+  result
+end 
   
