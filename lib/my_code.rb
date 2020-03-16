35da5883ae1015ec5e5380d70025a502a4bae42f
Steven Kandow <kando1sr@gmail.com>
2020-03-16 16:54:23 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,79 @@
 # My Code here....
+def map_to_negativize(source_array)
+  array_count = 0 
+  result = []
+  while array_count < source_array.length do 
+    result[array_count] = source_array[array_count] * -1
+    array_count += 1 
+  end
+  result
+end
+
+def map_to_no_change(source_array)
+  source_array
+end 
+
+def map_to_double(source_array)
+  array_count = 0 
+  result = []
+  while array_count < source_array.length do 
+    result[array_count] = source_array[array_count] * 2
+    array_count += 1 
+  end 
+  result
+end 
+
+def map_to_square(source_array)
+  array_count = 0 
+  result = []
+  while array_count < source_array.length do 
+    result[array_count] = source_array[array_count] ** 2 
+    array_count += 1 
+  end 
+  result
+end 
+
+def reduce_to_total(source_array, starting_value = 0)
+  result = starting_value 
+  array_count = 0 
+  while array_count < source_array.length do
+    result += source_array[array_count]
+    array_count += 1 
+  end 
+  result
+end 
+
+def reduce_to_all_true(source_array)
+  truth_count = 0 
+  array_count = 0 
+  while array_count < source_array.length do 
+    if source_array[array_count] != false 
+      truth_count += 1 
+    end 
+    array_count += 1 
+  end 
+  if truth_count == array_count
+    verdict = true 
+  else 
+    verdict = false
+  end 
+  verdict
+end 
+
+def reduce_to_any_true(source_array)
+  truth_count = 0 
+  array_count = 0 
+  while array_count < source_array.length do 
+    if source_array[array_count] != false && source_array[array_count] != nil
+      truth_count += 1 
+    end 
+    array_count += 1 
+  end 
+  if truth_count >= 1 
+    verdict = true 
+  else
+    verdict = false
+  end 
+  verdict
+end 
+  
